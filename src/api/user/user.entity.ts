3d07faa322050ba945315a65b7618edcdb3789bd
NineOne-Code <ihim512@gmail.com>
2022-03-19 11:46:27 +0700

    finished code but, not test on staging area

---
+++ src/api/user/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column({ type: 'varchar', length: 120 })
+  public name: string;
+
+  @Column({ type: 'varchar', length: 120 })
+  public email: string;
+
+  @Column({ type: 'boolean', default: false })
+  public isDeleted: boolean;
+
+  /*
+   * Create and Update Date Columns
+   */
+
+  @CreateDateColumn({ type: 'timestamp' })
+  public createdAt!: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  public updatedAt!: Date;
+}
